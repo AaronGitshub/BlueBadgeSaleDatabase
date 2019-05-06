4b3219b1f0b4a2150645b93c39148eec3c46dbcf
AaronGitsHub <aaronwood5@mac.com>
2019-05-06 11:16:52 -0400

    Add project files.

---
+++ SaleDatabaseMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SaleDatabaseMVC.MvcApplication" Language="C#" %>
---
+++ SaleDatabaseMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SaleDatabaseMVC.Startup))]
+namespace SaleDatabaseMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
