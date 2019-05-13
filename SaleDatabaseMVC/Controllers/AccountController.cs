3d05e86d92b4ebcb58f8cd94d577cda3cd65deb1
AaronGitsHub <aaronwood5@mac.com>
2019-05-13 14:06:26 -0400

    Added Dropdown to User Registration Page

--- SaleDatabaseMVC/Controllers/AccountController.cs
+++ SaleDatabaseMVC/Controllers/AccountController.cs
@@ -21,9 +21,10 @@ namespace SaleDatabaseMVC.Controllers
 
         public AccountController()
         {
+            var context = new ApplicationDbContext();
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -35,9 +36,9 @@ namespace SaleDatabaseMVC.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -121,7 +122,7 @@ namespace SaleDatabaseMVC.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -140,6 +141,7 @@ namespace SaleDatabaseMVC.Controllers
         [AllowAnonymous]
         public ActionResult Register()
         {
+            ViewBag.CompanyID = new SelectList(new ApplicationDbContext().Companies, "CompanyID", "CompanyName");
             return View();
         }
 
@@ -156,8 +158,8 @@ namespace SaleDatabaseMVC.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
