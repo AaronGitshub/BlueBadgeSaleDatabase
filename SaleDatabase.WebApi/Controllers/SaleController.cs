8b11dda12532c13bbe230f7b45f15a1bed7f3c10
AaronGitsHub <aaronwood5@mac.com>
2019-05-22 09:23:08 -0400

    AddedAPIController

---
+++ SaleDatabase.WebApi/Controllers/SaleController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity;
+using SaleDatabase.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace SaleDatabase.WebApi.Controllers
+{
+    [Authorize]
+    public class SaleController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            SaleService saleService = CreateSaleService();
+            var notes = saleService.GetSales();
+            return Ok(notes);
+        }
+        private SaleService CreateSaleService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var saleService = new SaleService(userId);
+            return saleService;
+        }
+    }
+}
